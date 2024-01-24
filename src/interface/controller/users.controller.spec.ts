e220506abc7d2195b64382e005944fae0fb4bed4
Tomoya <tomo16185@gmail.com>
2024-01-25 00:27:50 +0900

    :rotating_light: users.service.spec.tsのテストコード実装

--- src/interface/controller/users.controller.spec.ts
+++ src/interface/controller/users.controller.spec.ts
@@ -3,10 +3,12 @@ import { UsersController } from './users.controller';
 import { UsersService } from '../../usecases/users.service';
 import { User, PublicUser } from '../../domain/models/interface/user.interface';
 import { ConstantToken } from '../../enum/constant.token';
+import { it } from 'node:test';
 
 describe('UsersController', () => {
   let controller: UsersController;
   let fakeUsersService: Partial<UsersService>;
+
   const expectUsersResponse: PublicUser[] = [
     {
       id: 1,
@@ -26,6 +28,19 @@ describe('UsersController', () => {
             password: 'password',
           } as User,
         ]);
+      },
+      findUserByEmail: () => {
+        return Promise.resolve({
+          id: 1,
+          name: 'test',
+          email: 'sample@sample.com',
+          password: 'password',
+        } as User);
+      },
+      signIn: () => {
+        return Promise.resolve({
+          access_token: 'test-access-token',
+        });
       },
     };
 
@@ -50,5 +65,14 @@ describe('UsersController', () => {
     const users = await controller.users();
     expect(users.length).toEqual(1);
     expect(users).toEqual(expectUsersResponse);
+  });
+
+  it('ログイン成功後にアクセストークンが返ってくること', async () => {
+    const signInDto = {
+      email: 'sample@sample.com',
+      password: 'password',
+    };
+    const token = await controller.signIn(signInDto);
+    console.log(token);
   });
 });
--- src/usecases/users.service.spec.ts
+++ src/usecases/users.service.spec.ts
@@ -2,48 +2,56 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { User } from '../domain/models/interface/user.interface';
 import { ConstantToken } from '../enum/constant.token';
-import { UsersRepository } from '../infrastructure/repository/users.repository';
+import { JwtUserService } from '../usecases/jwt.user.service';
+import { BadRequestException } from '@nestjs/common';
 
 describe('UsersService', () => {
   let service: UsersService;
-  let fakeUsersRepository: Partial<UsersRepository>;
-  const expectUsersResponse: User[] = [
-    {
-      id: 1,
-      name: 'test',
-      email: 'sample@sample.com',
-      password: 'password',
+  let usersRepository;
+  let fakeJwtUserService: Partial<JwtUserService>;
+
+  const fakeUsersRepository = () => ({
+    users: () => {
+      return Promise.resolve([
+        {
+          id: 1,
+          name: 'test',
+          email: 'sample@sample.com',
+          password: 'password',
+        } as User,
+      ]);
     },
-  ];
+    findUserByEmail: jest.fn(),
+  });
+  const testUser: User = {
+    id: 1,
+    name: 'test',
+    email: 'sample@sample.com',
+    password: 'password',
+  };
+  const expectUsersResponse: User[] = [testUser];
+  const expectFindUserByEmailResponse: User = testUser;
 
   beforeEach(async () => {
-    fakeUsersRepository = {
-      users: () => {
-        return Promise.resolve([
-          {
-            id: 1,
-            name: 'test',
-            email: 'sample@sample.com',
-            password: 'password',
-          } as User,
-        ]);
-      },
-    };
-
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UsersService,
         {
           provide: ConstantToken.USERS_REPOSITORY,
-          useValue: fakeUsersRepository,
+          useFactory: fakeUsersRepository,
+        },
+        {
+          provide: ConstantToken.JWT_USER_SERVICE,
+          useValue: fakeJwtUserService,
         },
       ],
     }).compile();
 
+    usersRepository = module.get(ConstantToken.USERS_REPOSITORY);
     service = module.get<UsersService>(UsersService);
   });
 
-  it('Controllerが定義されているか', () => {
+  it('Serviceが定義されているか', () => {
     expect(service).toBeDefined();
   });
 
@@ -51,5 +59,18 @@ describe('UsersService', () => {
     const users = await service.users();
     expect(users.length).toEqual(1);
     expect(users).toEqual(expectUsersResponse);
+  });
+
+  it('emailに紐づくユーザーを取得する', async () => {
+    usersRepository.findUserByEmail.mockResolvedValue(testUser);
+    const user = await service.findUserByEmail('sample@sample.com');
+    expect(user).toEqual(expectFindUserByEmailResponse);
+  });
+
+  it('emailに紐づくユーザーが取得できない場合は例外をthrowする', async () => {
+    usersRepository.findUserByEmail.mockReturnValue(null);
+    await expect(
+      service.findUserByEmail('notfound@sample.com'),
+    ).rejects.toThrow(new BadRequestException());
   });
 });
